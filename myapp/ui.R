ed1c914288946f2b38bdec7e2cbc5728d9485f66
David-Munoz-B <43921136+David-Munoz-B@users.noreply.github.com>
2019-11-11 19:15:30 -0300

    Update ui.R

--- myapp/ui.R
+++ myapp/ui.R
@@ -1,25 +1,32 @@
-fluidPage(
-  # Application title
-  titlePanel("APP de Ledys y David"),
-  
-  sidebarLayout(
-    # Sidebar with a slider and selection inputs
-    sidebarPanel(
-      selectInput("selection", "Choose a book:",
-                  choices = books),
-      actionButton("update", "Change"),
-      hr(),
-      sliderInput("freq",
-                  "Minimum Frequency:",
-                  min = 1,  max = 50, value = 15),
-      sliderInput("max",
-                  "Maximum Number of Words:",
-                  min = 1,  max = 300,  value = 100)
-    ),
-    
-    # Show Word Cloud
-    mainPanel(
-      plotOutput("plot")
-    )
-  )
+ui <- dashboardPage(skin = "red",
+                    dashboardHeader(title = "ICHEM"),
+                    dashboardSidebar(
+                      sidebarMenu(
+                        br(),
+                        br(),
+                        br(),
+                        
+                        # menuItem("MAX & MIN", tabName = "M_MAXMIN"),
+                        # menuItem("RADAR", tabName = "M_RADAR"),
+                        hr(),
+                        "POR REGIÓN",br(),
+                        menuItem("ÍNDICE", tabName = "M_INDEX_R"),
+                        menuItem("DATOS", tabName = "M_DATA_R"),
+                        hr(),
+                        "TODO CHILE",br(),
+                        menuItem("ÍNDICE", tabName = "M_INDEX_CHILE"),
+                        menuItem("DATOS", tabName = "M_DATA_CHILE"),
+                        hr()
+                      )
+                    ),
+                    dashboardBody(
+                      # leafletOutput("mymap")
+                      tabItems(
+                        tabItem(tabName="M_INDEX_R", selectInput("INPUT_R", "Seleccione una región:", choices=unique(DATA_INDEX_RPC$NAME_REG)), parcoordsOutput("pc_R",height=700),style="height: 700px"),
+                        tabItem(tabName="M_DATA_R",selectInput("INPUT_R2", "Seleccione una región:", choices=unique(DATA_INDEX_RPC$NAME_REG)),DTOutput('DATA_INDEX_R')),
+                        tabItem(tabName="M_INDEX_CHILE",  parcoordsOutput("pc",height=4000),style="height: 4000px;"),
+                        tabItem(tabName="M_DATA_CHILE",DTOutput('DATA_INDEX_CHILE') )
+                        
+                      )
+                    )
 )
