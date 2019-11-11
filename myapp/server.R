d86c15035532a1b6ea2e6328aabb22f79c265177
David-Munoz-B <43921136+David-Munoz-B@users.noreply.github.com>
2019-11-11 19:15:03 -0300

    Update server.R

--- myapp/server.R
+++ myapp/server.R
@@ -1,32 +1,59 @@
-# Text of the books downloaded from:
-# A Mid Summer Night's Dream:
-#  http://www.gutenberg.org/cache/epub/2242/pg2242.txt
-# The Merchant of Venice:
-#  http://www.gutenberg.org/cache/epub/2243/pg2243.txt
-# Romeo and Juliet:
-#  http://www.gutenberg.org/cache/epub/1112/pg1112.txt
 
-function(input, output, session) {
-  # Define a reactive expression for the document term matrix
-  terms <- reactive({
-    # Change when the "update" button is pressed...
-    input$update
-    # ...but not for anything else
-    isolate({
-      withProgress({
-        setProgress(message = "Processing corpus...")
-        getTermMatrix(input$selection)
-      })
-    })
+server <- function(input, output){
+  
+  DATA_INDEX_RPC_R <- reactive(DATA_INDEX_RPC)
+  
+  output$pc <- renderParcoords({
+    parcoords(DATA_INDEX %>% dplyr::arrange(COD_COMUNA) %>% column_to_rownames('COD_COMUNA'), 
+              brushMode = "1D-axes-multi",
+              alpha=1, 
+              autoresize = T,
+              # height=3000,
+              color = list(colorBy = "I.PCA",colorScale = "scaleOrdinal")
+              , withD3 = TRUE)
   })
   
-  # Make the wordcloud drawing predictable during a session
-  wordcloud_rep <- repeatable(wordcloud)
-  
-  output$plot <- renderPlot({
-    v <- terms()
-    wordcloud_rep(names(v), v, scale=c(4,0.5),
-                  min.freq = input$freq, max.words=input$max,
-                  colors=brewer.pal(8, "Dark2"))
+  output$pc_R <- renderParcoords({
+    parcoords(DATA_INDEX_RPC %>% dplyr::arrange(COMUNA) %>% 
+                dplyr::filter(NAME_REG==input$INPUT_R) %>% 
+                dplyr::select(-NAME_REG,-NAME_PROV) %>% 
+                column_to_rownames('COMUNA'), 
+              brushMode = "1D-axes-multi",
+              alpha=1,
+              autoresize = T,
+              color = list(colorBy = "I.PCA",colorScale = "scaleOrdinal")
+              , withD3 = TRUE)
   })
+  
+  
+  
+  output$DATA_INDEX_R <- renderDT(
+    datatable(DATA_INDEX_RPC %>% dplyr::arrange(COMUNA) %>% 
+                dplyr::filter(NAME_REG==input$INPUT_R2) %>% 
+                dplyr::select(-NAME_REG,-NAME_PROV) %>% 
+                mutate(COMUNA=as.factor(COMUNA)), rownames = FALSE,
+              extensions = 'Buttons',filter = 'top',
+              options = list(scrollX = TRUE,pageLength = -1, dom = 'Bfrtip',
+                             buttons = c('copy', 'csv', 'excel')
+              )) %>% 
+      formatRound( c(2:ncol(DATA_INDEX_RPC)), 4)) 
+  
+  output$DATA_INDEX_CHILE <- renderDT(
+    datatable(DATA_INDEX %>% rename(COMUNA=COD_COMUNA) %>% 
+                dplyr::arrange(COMUNA) %>% 
+                mutate(COMUNA=as.factor(COMUNA)), rownames = FALSE,
+              extensions = 'Buttons',filter = 'top',
+              options = list(scrollX = TRUE,pageLength = -1, dom = 'Bfrtip',
+                             buttons = c('copy', 'csv', 'excel')
+              )) %>% 
+      formatRound( c(2:ncol(DATA_INDEX_RPC)), 4)) 
+  
+  # output$mymap <- renderLeaflet({
+  #   
+  #     leaflet(data = llanos) %>% addTiles() %>% addProviderTiles(providers$OpenStreetMap) %>% 
+  #     addPolygons(fill = FALSE, stroke = TRUE, color = "#03F") %>% addLegend("bottomright", colors = "#03F", labels = "Llanos ecoregion")
+  #   ,style="height: 700px;"
+  #   
+  #     
+  # })
 }
